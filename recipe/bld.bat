56d1d4c8e6105ada6200860e9c2240cff32b0710
Julien Schueller <schueller@phimeca.com>
2017-06-30 08:37:07 +0200

    Use CMAKE_CONFIG

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,9 +9,11 @@ mkdir buildw && cd buildw
 
 set CMAKE_CONFIG="Release"
 
-cmake -LAH -G"NMake Makefiles"                               ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..
+cmake -LAH -G"NMake Makefiles"                             ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                ^
+  -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"                      ^
+  ..
 if errorlevel 1 exit 1
 
-cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+cmake --build . --config %CMAKE_CONFIG% --target install
 if errorlevel 1 exit 1
