9ad88f51096d63ef3abf9550d278f928bf708871
Julien Schueller <schueller@phimeca.com>
2017-09-26 10:21:22 +0200

    Enable ctest on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,9 +12,14 @@ set CMAKE_CONFIG="Release"
 cmake -LAH -G"NMake Makefiles"                             ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                ^
   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"                      ^
-  -DTBB_BUILD_TESTS=OFF                                    ^
+  -DTBB_CI_BUILD=ON                                        ^
   ..
 if errorlevel 1 exit 1
 
 cmake --build . --config %CMAKE_CONFIG% --target install
 if errorlevel 1 exit 1
+
+
+ctest -C %CMAKE_CONFIG% --output-on-failure --timeout 500
+if errorlevel 1 exit 1
+
